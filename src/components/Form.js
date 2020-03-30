bdc9e2d6ed8a6aad36370301d265193f879f96c7
de-ladvez <deladvez69@gmail.com>
2020-03-31 02:18:25 +0300

    dateFormatForChart
    add data for chart

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,62 +1,66 @@
 import React from "react";
-import { useStore } from 'react-redux'
+import { useStore } from "react-redux";
 
 import CSVReader from "react-csv-reader";
 import "./form.css";
-import { useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Forms = ({
-                   csvList,
-                   setItemCsv,
-                   checkToEmptyDataItemCsv
+  csvList,
+  setItemCsv,
+  checkToEmptyDataItemCsv,
+  updateDataForChartItemCsv
 }) => {
-    let history = useHistory();
-    const store = useStore();
-    const papaparseOptions = {
-        header: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-        transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
-    };
+  let history = useHistory();
+  const store = useStore();
+  const papaparseOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
+  };
 
-    function handleSendData() {
-        checkToEmptyDataItemCsv().then(res => {
-            history.push("/chart");
+  function handleSendData() {
+    checkToEmptyDataItemCsv().then(res => {
+      updateDataForChartItemCsv();
+      history.push("/chart");
 
-            for(let item of store.getState().csvList) {
-                if(!item.active) {
-                    return;
-                }
+      for (let item of store.getState().csvList) {
+        if (!item.active) {
+          return;
+        }
+      }
+      console.log("OK!");
+    });
+  }
+
+  return (
+    <div className="loadFiles-row">
+      {csvList.map((item, name) => (
+        <div
+          className={["loadFiles-item", item.active ? "" : "empty"].join(" ")}
+          key={item.name}
+        >
+          <CSVReader
+            label={item.nameFile || `Select 'csv' file with ${item.name} data`}
+            inputId={item.name}
+            onFileLoaded={(data, fileInfo) =>
+              setItemCsv({
+                ...item,
+                data,
+                nameFile: fileInfo.name,
+                active: 1
+              })
             }
-            console.log("OK!");
-        });
-    }
-
-    return (
-        <div className="loadFiles-row">
-            {csvList.map((item, name) => (
-                <div className={["loadFiles-item", item.active ? "" : "empty"].join(" ")} key={item.name}>
-                    <CSVReader
-                        label={item.nameFile || `Select 'csv' file with ${item.name} data`}
-                        inputId={item.name}
-                        onFileLoaded={
-                            (data, fileInfo) => setItemCsv({
-                                ...item,
-                                data,
-                                nameFile: fileInfo.name,
-                                active: 1
-                            })
-                        }
-                        parserOptions={papaparseOptions}
-                    />
-                </div>
-            ))}
-            <div className="loadFiles-button" onClick={handleSendData}>
-                Open charts and 3d vue
-            </div>
+            parserOptions={papaparseOptions}
+          />
         </div>
-    );
-
+      ))}
+      <div className="loadFiles-button" onClick={handleSendData}>
+        Open charts and 3d vue
+      </div>
+    </div>
+  );
 };
 
 export default Forms;
--- src/reduser/reduser.js
+++ src/reduser/reduser.js
@@ -1,76 +1,93 @@
 import {
-    SET_ITEM_CSV,
-    CHECK_TO_EMPTY_DATA_ITEM_CSV,
-    UPDATE_DATA_FOR_CHARTS_ITEM_CSV
-} from '../action/action';
-import { combineReducers  } from "redux";
+  SET_ITEM_CSV,
+  CHECK_TO_EMPTY_DATA_ITEM_CSV,
+  UPDATE_DATA_FOR_CHARTS_ITEM_CSV
+} from "../action/action";
+import { combineReducers } from "redux";
+import dateFormat from "dateformat";
 
 const initialState = [
-    {
-        name: "gForce",
-        data: [],
-        nameFile: "",
-        active: 2,
-        typeForChart: [
-            ["time", "gfx"],
-            ["time", "gfy"],
-            ["time", "gfz"],
-            ["time", "gftotal"]
-        ]
-    },
-    {
-        name: "acelerometer",
-        data: [],
-        nameFile: "",
-        active: 2,
-        typeForChart: []
-    },
-    {
-        name: "gyroscope",
-        data: [],
-        nameFile: "",
-        active: 2,
-        typeForChart: []
-    },
-    {
-        name: "gFobarometerrce",
-        data: [],
-        nameFile: "",
-        active: 2,
-        typeForChart: []
-    },
-    {
-        name: "gps",
-        data: [],
-        nameFile: "",
-        active: 2,
-        typeForChart: [
-            ["time", "latitude"],
-            ["time", "longitude"]
-        ]
-    },
+  {
+    name: "gForce",
+    data: [],
+    nameFile: "",
+    active: 2,
+    typeForChart: [
+      ["time", "gfx"],
+      ["time", "gfy"],
+      ["time", "gfz"],
+      ["time", "gftotal"]
+    ]
+  },
+  {
+    name: "acelerometer",
+    data: [],
+    nameFile: "",
+    active: 2,
+    typeForChart: []
+  },
+  {
+    name: "gyroscope",
+    data: [],
+    nameFile: "",
+    active: 2,
+    typeForChart: []
+  },
+  {
+    name: "gFobarometerrce",
+    data: [],
+    nameFile: "",
+    active: 2,
+    typeForChart: []
+  },
+  {
+    name: "gps",
+    data: [],
+    nameFile: "",
+    active: 2,
+    typeForChart: [["time", "latitude"], ["time", "longitude"]]
+  }
 ];
 
 function csvList(state = initialState, action) {
-    switch (action && action.type) {
-        case SET_ITEM_CSV:
-            return (state.map(item => item.name === action.data.name ? action.data : item));
-        case CHECK_TO_EMPTY_DATA_ITEM_CSV:
-            return (state.map(item => !item.data.length ? {...item, active: 0} : item));
-        case UPDATE_DATA_FOR_CHARTS_ITEM_CSV:
-            //TODO: закончил тут
-            return ;
-        default:
-            return state
-
-    }
+  switch (action && action.type) {
+    case SET_ITEM_CSV:
+      return state.map(item =>
+        item.name === action.data.name ? action.data : item
+      );
+    case CHECK_TO_EMPTY_DATA_ITEM_CSV:
+      return state.map(item =>
+        !item.data.length ? { ...item, active: 0 } : item
+      );
+    case UPDATE_DATA_FOR_CHARTS_ITEM_CSV:
+      const res = state.map(item => {
+        if (!item.data.length) {
+          return item;
+        }
+        let initPairsForChart = {};
+        for (let init of item.typeForChart) {
+          initPairsForChart[init[1]] = [];
+        }
+        const dataChatr = item.data.reduce((acum, csvCol) => {
+          let datef = dateFormat(
+            csvCol[item.typeForChart[0][0]],
+            "yyyy-mm-dd HH.MM.ss"
+          );
+          for (let i of item.typeForChart) {
+            acum[i[1]].push([datef, csvCol[i[1]]]);
+          }
+          return acum;
+        }, initPairsForChart);
+        return { ...item, dataChatr };
+      });
+      return res;
+    default:
+      return state;
+  }
 }
 
-
-
-
 const reduserApp = combineReducers({
-    csvList
+  csvList
 });
 
 export default reduserApp;
